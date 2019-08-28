6a8d0cad9060cba0999137bff194a9eec7c83e08
rajasd27 <rajasdeshpande27@gmail.com>
2019-08-27 20:18:27 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,4 +3,3 @@ start ServerPrototype.exe ../Project4 localhost 8080
 
 cd ..\ClientGUI\bin\x86\Debug\
 start ClientGUI.exe ../../Project4 localhost 8080 8081
-start ClientGUI.exe ../../Project4 localhost 8080 8082
